e7b39b402862dec56d2fa1ec4f8e1475558ed4e4
Dmitro Kuzmin <Dmitrikkuzmin@gmail.com>
2022-12-24 01:13:43 +0200

    add input with a new parameter

--- first_steps/input_print.py
+++ first_steps/input_print.py
@@ -8,3 +8,6 @@ print("------------------------------")
 print('What is your name?')
 name = input()
 print('Hello,', name)
+
+name = input("What is your name?\n") # Передаем текст как параметр, \n - перенос на другую строку
+print('Hello,', name)
