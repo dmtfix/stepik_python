92187e8387c66294c599eb77947e67b17091f6c6
Dmitro Kuzmin <Dmitrikkuzmin@gmail.com>
2022-12-24 04:13:06 +0200

    Additional operations(add small task)

--- first_steps/integers.py
+++ first_steps/integers.py
@@ -32,3 +32,14 @@ available = mandarins // students
 basket = mandarins % students
 print(available)
 print(basket)
+
+"""Безумный титан Танос собрал все 6 камней бесконечности и намеревается 
+уничтожить половину населения Вселенной по щелчку пальцев.
+При этом если население Вселенной является нечетным числом,
+то титан проявит милосердие и округлит количество выживших в большую сторону. 
+Помогите Мстителям подсчитать количество выживших."""
+
+population = int(input())
+balance = population % 2
+remainders = (population + balance) // 2
+print(remainders)
