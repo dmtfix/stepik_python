d391cad002e60984812bd36435c8fa436fceaaef
Dmitro Kuzmin <Dmitrikkuzmin@gmail.com>
2022-12-25 07:54:24 +0200

    Add exercise #2

--- operator_if_else/exercises_logical_operators.py
+++ operator_if_else/exercises_logical_operators.py
@@ -8,3 +8,15 @@ if (1000 <= num <= 9999 and num % 7 == 0) or (1000 <= num <= 9999 and num % 17 =
     print("YES")
 else:
     print("NO")
+
+"""Напишите программу, которая принимает три положительных числа и определяет,
+ существует ли невырожденный треугольник с такими сторонами."""
+
+side1 = int(input())
+side2 = int(input())
+side3 = int(input())
+
+if (side1 < (side2 + side3)) and (side2 < (side1 + side3)) and (side3 < (side1 + side2)):
+    print("YES")
+else:
+    print("NO")
